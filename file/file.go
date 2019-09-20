f975efec1bebf693b42fe3e448763fd9aaaee897
Vadim Kilibaba <vkulibaba@surfy.ru>
2019-09-20 17:35:39 +0300

    file

--- file/file.go
+++ file/file.go
@@ -2,8 +2,9 @@ package file
 
 import "os"
 
+// WriteAtPos write at and or pos and retun seek
 // if pos<0 store at the end of file
-func writeAtPos(f *os.File, b []byte, pos int64) (seek int64, n int, err error) {
+func WriteAtPos(f *os.File, b []byte, pos int64) (seek int64, n int, err error) {
 	seek = pos
 	if pos < 0 {
 		seek, err = f.Seek(0, 2)
