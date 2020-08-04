001bb0a43e1179f1c2b2a27ae2574e80bcf57c6b
recoilme <vadim-kulibaba@yandex.ru>
2020-08-04 23:19:57 +0300

    fix fsync

---
+++ chunk.go
@@ -0,0 +1,321 @@
+package sniper
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"io"
+	"os"
+	"sync"
+)
+
+// chunk - local shard
+type chunk struct {
+	sync.RWMutex
+	f         *os.File            // file storage
+	m         map[uint32]addrSize // keys: hash / addr&len
+	h         map[uint32]byte     // holes: addr / size
+	needFsync bool
+}
+
+func packetMarshal(k, v []byte) (sizeb byte, b []byte) {
+	// write head
+	sizeb, size := NextPowerOf2(uint32(len(v) + len(k) + sizeHead))
+	b = make([]byte, size)
+	b[0] = sizeb
+	b[1] = byte(0)
+	//len key in bigendian format
+	lenk := uint16(len(k))
+	b[2] = byte(lenk >> 8)
+	b[3] = byte(lenk)
+	//len val in bigendian format
+	lenv := uint32(len(v))
+	b[4] = byte(lenv >> 24)
+	b[5] = byte(lenv >> 16)
+	b[6] = byte(lenv >> 8)
+	b[7] = byte(lenv)
+	// write body: val and key
+	copy(b[sizeHead:], v)
+	copy(b[sizeHead+lenv:], k)
+	return
+}
+
+func packetUnmarshal(packet []byte) (k, v []byte, sizeb byte) {
+	_ = packet[7]
+	sizeb = packet[0]
+	lenk := binary.BigEndian.Uint16(packet[2:4])
+	lenv := binary.BigEndian.Uint32(packet[4:8])
+	k = packet[sizeHead+lenv : sizeHead+lenv+uint32(lenk)]
+	v = packet[sizeHead : sizeHead+lenv]
+	return
+}
+
+func (c *chunk) init(name string) (err error) {
+	c.Lock()
+	defer c.Unlock()
+
+	f, err := os.OpenFile(name, os.O_CREATE|os.O_RDWR, os.FileMode(fileMode))
+	if err != nil {
+		return
+	}
+	err = f.Sync()
+	if err != nil {
+		return err
+	}
+	c.f = f
+	c.m = make(map[uint32]addrSize)
+	c.h = make(map[uint32]byte)
+	//read if f not empty
+	if fi, e := c.f.Stat(); e == nil {
+		if fi.Size() == 0 {
+			return
+		}
+		//read file
+		var seek int
+		for {
+			b := make([]byte, 8)
+			n, errRead := c.f.Read(b)
+			if errRead != nil || n != 8 {
+				if errRead != nil && errRead.Error() != "EOF" {
+					err = errRead
+				}
+				break
+			}
+			//readed header
+			lenk := binary.BigEndian.Uint16(b[2:4])
+			lenv := binary.BigEndian.Uint32(b[4:8])
+			if lenk == 0 {
+				return ErrFormat
+			}
+			// skip val
+			_, seekerr := c.f.Seek(int64(lenv), 1)
+			if seekerr != nil {
+				return ErrFormat
+			}
+			// read key
+			key := make([]byte, lenk)
+			n, errRead = c.f.Read(key)
+			if errRead != nil || n != int(lenk) {
+				return ErrFormat
+			}
+			shiftv := 1 << byte(b[0])                                               //2^pow
+			ret, seekerr := c.f.Seek(int64(shiftv-int(lenk)-int(lenv)-sizeHead), 1) // skip val && key
+			if seekerr != nil {
+				return ErrFormat
+			}
+			// map store
+			if b[1] != deleted {
+				h := hash(key)
+				c.m[h] = addrSizeMarshal(uint32(seek), b[0])
+			} else {
+				//deleted blocks store
+				c.h[uint32(seek)] = b[0] // seek / size
+			}
+			seek = int(ret)
+		}
+	}
+
+	return
+}
+
+//fsync commits the current contents of the file to stable storage
+func (c *chunk) fsync() error {
+	if c.needFsync {
+		c.Lock()
+		c.needFsync = false
+		c.Unlock()
+		return c.f.Sync()
+	}
+	return nil
+}
+
+// set - write data to file & in map
+func (c *chunk) set(k, v []byte, h uint32) (err error) {
+	c.Lock()
+	defer c.Unlock()
+	c.needFsync = true
+	sizeb, b := packetMarshal(k, v)
+	// write at file
+	pos := int64(-1)
+
+	if addrsize, ok := c.m[h]; ok {
+		addr, size := addrSizeUnmarshal(addrsize)
+		packet := make([]byte, size)
+		_, err = c.f.ReadAt(packet, int64(addr))
+		if err != nil {
+			return err
+		}
+		key, _, sizeold := packetUnmarshal(packet)
+		if !bytes.Equal(key, k) {
+			//println(string(key), string(k))
+			return ErrCollision
+		}
+
+		if err == nil && sizeold == sizeb {
+			//overwrite
+			pos = int64(addr)
+		} else {
+			// mark old k/v as deleted
+			delb := make([]byte, 1)
+			delb[0] = deleted
+			_, err = c.f.WriteAt(delb, int64(addr+1))
+			if err != nil {
+				return err
+			}
+			c.h[addr] = sizeold
+
+			// try to find optimal empty hole
+			for addrh, sizeh := range c.h {
+				if sizeh == sizeb {
+					pos = int64(addrh)
+					delete(c.h, addrh)
+					break
+				}
+			}
+		}
+	}
+	// write at end or in hole or overwrite
+	if pos < 0 {
+		pos, err = c.f.Seek(0, 2) // append to the end of file
+	}
+	_, err = c.f.WriteAt(b, pos)
+	if err != nil {
+		return err
+	}
+	c.m[h] = addrSizeMarshal(uint32(pos), sizeb)
+	return
+}
+
+// get return val by key
+func (c *chunk) get(k []byte, h uint32) (v []byte, err error) {
+	c.RLock()
+	defer c.RUnlock()
+	if addrsize, ok := c.m[h]; ok {
+		addr, size := addrSizeUnmarshal(addrsize)
+		packet := make([]byte, size)
+		_, err = c.f.ReadAt(packet, int64(addr))
+		if err != nil {
+			return
+		}
+		key, val, _ := packetUnmarshal(packet)
+		if !bytes.Equal(key, k) {
+			return nil, ErrCollision
+		}
+		v = val
+	} else {
+		return nil, ErrNotFound
+	}
+	return
+}
+
+// return map length
+func (c *chunk) count() int {
+	c.RLock()
+	defer c.RUnlock()
+	return len(c.m)
+}
+
+// close file
+func (c *chunk) close() (err error) {
+	c.Lock()
+	defer c.Unlock()
+	return c.f.Close()
+}
+
+func (c *chunk) fileSize() (int64, error) {
+	c.Lock()
+	defer c.Unlock()
+	is, err := c.f.Stat()
+	if err != nil {
+		return -1, err
+	}
+	return is.Size(), nil
+}
+
+// delete mark item as deleted at specified position
+func (c *chunk) delete(k []byte, h uint32) (isDeleted bool, err error) {
+	c.Lock()
+	defer c.Unlock()
+	if addrsize, ok := c.m[h]; ok {
+		addr, size := addrSizeUnmarshal(addrsize)
+		packet := make([]byte, size)
+		_, err = c.f.ReadAt(packet, int64(addr))
+		if err != nil {
+			return
+		}
+		key, _, sizeb := packetUnmarshal(packet)
+		if !bytes.Equal(key, k) {
+			return false, ErrCollision
+		}
+
+		delb := make([]byte, 1)
+		delb[0] = deleted
+		_, err = c.f.WriteAt(delb, int64(addr+1))
+		if err != nil {
+			return
+		}
+		delete(c.m, h)
+		c.h[addr] = sizeb
+		isDeleted = true
+	}
+	return
+}
+
+// TODO - optimize
+func (c *chunk) incrdecr(k []byte, h uint32, v uint64, isIncr bool) (counter uint64, err error) {
+	mutex.Lock()
+	defer mutex.Unlock()
+	old, err := c.get(k, h)
+	if err == ErrNotFound {
+		//create empty counter
+		old = make([]byte, 8)
+		err = nil
+	}
+	if len(old) != 8 {
+		//better, then panic
+		return 0, errors.New("Unexpected value format")
+	}
+	if err != nil {
+		return
+	}
+	counter = binary.BigEndian.Uint64(old)
+	if isIncr {
+		counter += v
+	} else {
+		//decr
+		counter -= v
+	}
+	new := make([]byte, 8)
+	binary.BigEndian.PutUint64(new, counter)
+	err = c.set(k, new, h)
+
+	return
+}
+
+func (c *chunk) backup() (err error) {
+	c.Lock()
+	defer c.Unlock()
+	name := c.f.Name() + ".bak"
+	os.Remove(name)
+
+	dest, err := os.OpenFile(name, os.O_CREATE|os.O_RDWR, os.FileMode(fileMode))
+	if err != nil {
+		return
+	}
+	err = c.f.Sync()
+	if err != nil {
+		return
+	}
+	_, err = c.f.Seek(0, 0)
+	if err != nil {
+		return
+	}
+	if _, err = io.Copy(dest, c.f); err != nil {
+		return
+	}
+	err = dest.Sync()
+	if err != nil {
+		return
+	}
+	return dest.Close()
+}
--- sniper.go
+++ sniper.go
@@ -1,11 +1,8 @@
 package sniper
 
 import (
-	"bytes"
-	"encoding/binary"
 	"errors"
 	"fmt"
-	"io"
 	"os"
 	"sync"
 	"time"
@@ -21,13 +18,16 @@ const fileMode = 0666
 const dirMode = 0777
 const sizeHead = 8
 const deleted = 42 // flag for removed, tribute 2 dbf!
+
 //ErrCollision -  must not happen
 var ErrCollision = errors.New("Error, hash collision")
 
+// ErrFormat unexpected file format
 var ErrFormat = errors.New("Error, unexpected file format")
+
+// ErrNotFound key not found error
 var ErrNotFound = errors.New("Error, key not found")
 
-//var ErrCollision = errors.New("Error, hash collision") // don't happen, on top layer of database
 var counters sync.Map
 var mutex = &sync.RWMutex{} //global mutex for counters and so on
 
@@ -37,6 +37,7 @@ type Store struct {
 	chunks       [chunksCnt]chunk
 	dir          string
 	syncInterval time.Duration
+	iv           interval.Interval
 }
 
 type addrSize struct {
@@ -44,28 +45,11 @@ type addrSize struct {
 	size byte
 }
 
-// chunk
-type chunk struct {
-	sync.RWMutex
-	f  *os.File            // file storage
-	m  map[uint32]addrSize // keys: hash / addr&len
-	h  map[uint32]byte     // holes: addr / size
-	iv interval.Interval
-}
-
-// Option is a function that takes a pointer to a Store and returns an error if it is invalid
-type Option func(*Store) error
-
-// SyncInterval - how often fsync do, default 0 = disabled
-func SyncInterval(interv time.Duration) Option {
-	return func(s *Store) error {
-		s.syncInterval = interv
-		return nil
-	}
-}
+// OptStore is a store options
+type OptStore func(*Store) error
 
 // Dir - directory for database, default "."
-func Dir(dir string) Option {
+func Dir(dir string) OptStore {
 	return func(s *Store) error {
 		if dir == "" {
 			dir = "."
@@ -91,6 +75,26 @@ func Dir(dir string) Option {
 	}
 }
 
+// SyncInterval - how often fsync do, default 0 - OS will do it
+func SyncInterval(interv time.Duration) OptStore {
+	return func(s *Store) error {
+		s.syncInterval = interv
+		if interv > 0 {
+			s.iv = interval.Set(func(t time.Time) {
+				for i := range s.chunks[:] {
+					err := s.chunks[i].fsync()
+					if err != nil {
+						fmt.Printf("Error fsync:%s\n", err)
+						//critical error, ssd/hdd drive is broken
+						panic(err)
+					}
+				}
+			}, interv)
+		}
+		return nil
+	}
+}
+
 func hash(b []byte) uint32 {
 	return murmur3.Sum32WithSeed(b, 0)
 	/*
@@ -101,90 +105,13 @@ func hash(b []byte) uint32 {
 	*/
 }
 
-func (c *chunk) init(name string, syncInterval time.Duration) (err error) {
-	c.Lock()
-	defer c.Unlock()
-
-	f, err := os.OpenFile(name, os.O_CREATE|os.O_RDWR, os.FileMode(fileMode))
-	if err != nil {
-		return
-	}
-	err = f.Sync()
-	if err != nil {
-		return err
-	}
-	c.f = f
-	c.m = make(map[uint32]addrSize)
-	c.h = make(map[uint32]byte)
-
-	if syncInterval>0 {
-		c.iv = interval.Set(func(t time.Time) {
-			err = c.f.Sync()
-			if err != nil {
-				//not possible, if drive ok, may be panic here??
-				fmt.Printf("Error fsync:%s\n", err)
-			}
-		}, syncInterval)
-	}
-	//read if f not empty
-	if fi, e := c.f.Stat(); e == nil {
-		if fi.Size() == 0 {
-			return
-		}
-		//read file
-		var seek int
-		for {
-			b := make([]byte, 8)
-			n, errRead := c.f.Read(b)
-			if errRead != nil || n != 8 {
-				if errRead != nil && errRead.Error() != "EOF" {
-					err = errRead
-				}
-				break
-			}
-			//readed header
-			lenk := binary.BigEndian.Uint16(b[2:4])
-			lenv := binary.BigEndian.Uint32(b[4:8])
-			if lenk == 0 {
-				return ErrFormat
-			}
-			// skip val
-			_, seekerr := c.f.Seek(int64(lenv), 1)
-			if seekerr != nil {
-				return ErrFormat
-			}
-			// read key
-			key := make([]byte, lenk)
-			n, errRead = c.f.Read(key)
-			if errRead != nil || n != int(lenk) {
-				return ErrFormat
-			}
-			shiftv := 1 << byte(b[0])                                               //2^pow
-			ret, seekerr := c.f.Seek(int64(shiftv-int(lenk)-int(lenv)-sizeHead), 1) // skip val && key
-			if seekerr != nil {
-				return ErrFormat
-			}
-			// map store
-			if b[1] != deleted {
-				h := hash(key)
-				c.m[h] = addrSizeMarshal(uint32(seek), b[0])
-			} else {
-				//deleted blocks store
-				c.h[uint32(seek)] = b[0] // seek / size
-			}
-			seek = int(ret)
-		}
-	}
-
-	return
-}
-
 // Open return new store
 // It will create 256 shards
 // Each shard store keys and val size and address in map[uint32]uint32
 //
 // options, see https://gist.github.com/travisjeffery/8265ca411735f638db80e2e34bdbd3ae#gistcomment-3171484
-func Open(opts ...Option) (s *Store, err error) {
+// usage - Open(Dir("1"), SyncInterval(1*time.Second))
+func Open(opts ...OptStore) (s *Store, err error) {
 	s = &Store{}
 
 	s.syncInterval = 0
@@ -200,7 +127,7 @@ func Open(opts ...Option) (s *Store, err error) {
 	// create chuncks
 	for i := range s.chunks[:] {
 
-		err = s.chunks[i].init(fmt.Sprintf("%s/%d", s.dir, i),s.syncInterval)
+		err = s.chunks[i].init(fmt.Sprintf("%s/%d", s.dir, i))
 		if err != nil {
 			return nil, err
 		}
@@ -222,38 +149,6 @@ func idx(h uint32) uint32 {
 	return (h % (chunksCnt - chunkColCnt)) + chunkColCnt
 }
 
-func packetMarshal(k, v []byte) (sizeb byte, b []byte) {
-	// write head
-	sizeb, size := NextPowerOf2(uint32(len(v) + len(k) + sizeHead))
-	b = make([]byte, size)
-	b[0] = sizeb
-	b[1] = byte(0)
-	//len key in bigendian format
-	lenk := uint16(len(k))
-	b[2] = byte(lenk >> 8)
-	b[3] = byte(lenk)
-	//len val in bigendian format
-	lenv := uint32(len(v))
-	b[4] = byte(lenv >> 24)
-	b[5] = byte(lenv >> 16)
-	b[6] = byte(lenv >> 8)
-	b[7] = byte(lenv)
-	// write body: val and key
-	copy(b[sizeHead:], v)
-	copy(b[sizeHead+lenv:], k)
-	return
-}
-
-func packetUnmarshal(packet []byte) (k, v []byte, sizeb byte) {
-	_ = packet[7]
-	sizeb = packet[0]
-	lenk := binary.BigEndian.Uint16(packet[2:4])
-	lenv := binary.BigEndian.Uint32(packet[4:8])
-	k = packet[sizeHead+lenv : sizeHead+lenv+uint32(lenk)]
-	v = packet[sizeHead : sizeHead+lenv]
-	return
-}
-
 // Set - store key and val in shard
 // max packet size is 2^19, 512kb (524288)
 // packet size = len(key) + len(val) + 8
@@ -270,62 +165,6 @@ func (s *Store) Set(k, v []byte) (err error) {
 			break
 		}
 	}
-	return
-}
-
-// set - write data to file & in map
-func (c *chunk) set(k, v []byte, h uint32) (err error) {
-	c.Lock()
-	defer c.Unlock()
-	sizeb, b := packetMarshal(k, v)
-	// write at file
-	pos := int64(-1)
-
-	if addrsize, ok := c.m[h]; ok {
-		addr, size := addrSizeUnmarshal(addrsize)
-		packet := make([]byte, size)
-		_, err = c.f.ReadAt(packet, int64(addr))
-		if err != nil {
-			return err
-		}
-		key, _, sizeold := packetUnmarshal(packet)
-		if !bytes.Equal(key, k) {
-			//println(string(key), string(k))
-			return ErrCollision
-		}
-
-		if err == nil && sizeold == sizeb {
-			//overwrite
-			pos = int64(addr)
-		} else {
-			// mark old k/v as deleted
-			delb := make([]byte, 1)
-			delb[0] = deleted
-			_, err = c.f.WriteAt(delb, int64(addr+1))
-			if err != nil {
-				return err
-			}
-			c.h[addr] = sizeold
-
-			// try to find optimal empty hole
-			for addrh, sizeh := range c.h {
-				if sizeh == sizeb {
-					pos = int64(addrh)
-					delete(c.h, addrh)
-					break
-				}
-			}
-		}
-	}
-	// write at end or in hole or overwrite
-	if pos < 0 {
-		pos, err = c.f.Seek(0, 2) // append to the end of file
-	}
-	_, err = c.f.WriteAt(b, pos)
-	if err != nil {
-		return err
-	}
-	c.m[h] = addrSizeMarshal(uint32(pos), sizeb)
 	return
 }
 
@@ -346,28 +185,6 @@ func (s *Store) Get(k []byte) (v []byte, err error) {
 	return
 }
 
-// get return val by key
-func (c *chunk) get(k []byte, h uint32) (v []byte, err error) {
-	c.RLock()
-	defer c.RUnlock()
-	if addrsize, ok := c.m[h]; ok {
-		addr, size := addrSizeUnmarshal(addrsize)
-		packet := make([]byte, size)
-		_, err = c.f.ReadAt(packet, int64(addr))
-		if err != nil {
-			return
-		}
-		key, val, _ := packetUnmarshal(packet)
-		if !bytes.Equal(key, k) {
-			return nil, ErrCollision
-		}
-		v = val
-	} else {
-		return nil, ErrNotFound
-	}
-	return
-}
-
 // Count return count keys
 func (s *Store) Count() (cnt int) {
 	for i := range s.chunks[:] {
@@ -376,16 +193,12 @@ func (s *Store) Count() (cnt int) {
 	return
 }
 
-// return map length
-func (c *chunk) count() int {
-	c.RLock()
-	defer c.RUnlock()
-	return len(c.m)
-}
-
 // Close - close related chunks
 func (s *Store) Close() (err error) {
 	errStr := ""
+	if s.syncInterval > 0 {
+		s.iv.Clear()
+	}
 	for i := range s.chunks[:] {
 		err = s.chunks[i].close()
 		if err != nil {
@@ -397,15 +210,6 @@ func (s *Store) Close() (err error) {
 		return errors.New(errStr)
 	}
 	return
-}
-
-// close file
-func (c *chunk) close() (err error) {
-	c.Lock()
-	defer c.Unlock()
-	//c.iv.Clear()
-
-	return c.f.Close()
 }
 
 // DeleteStore - remove directory with files
@@ -423,16 +227,6 @@ func (s *Store) FileSize() (fs int64, err error) {
 		fs += is
 	}
 	return
-}
-
-func (c *chunk) fileSize() (int64, error) {
-	c.Lock()
-	defer c.Unlock()
-	is, err := c.f.Stat()
-	if err != nil {
-		return -1, err
-	}
-	return is.Size(), nil
 }
 
 // https://github.com/thejerf/gomempool/blob/master/pool.go#L519
@@ -487,35 +281,6 @@ func (s *Store) Delete(k []byte) (isDeleted bool, err error) {
 	return
 }
 
-// delete mark item as deleted at specified position
-func (c *chunk) delete(k []byte, h uint32) (isDeleted bool, err error) {
-	c.Lock()
-	defer c.Unlock()
-	if addrsize, ok := c.m[h]; ok {
-		addr, size := addrSizeUnmarshal(addrsize)
-		packet := make([]byte, size)
-		_, err = c.f.ReadAt(packet, int64(addr))
-		if err != nil {
-			return
-		}
-		key, _, sizeb := packetUnmarshal(packet)
-		if !bytes.Equal(key, k) {
-			return false, ErrCollision
-		}
-
-		delb := make([]byte, 1)
-		delb[0] = deleted
-		_, err = c.f.WriteAt(delb, int64(addr+1))
-		if err != nil {
-			return
-		}
-		delete(c.m, h)
-		c.h[addr] = sizeb
-		isDeleted = true
-	}
-	return
-}
-
 // Incr - Incr item by uint64
 // inited with zero
 func (s *Store) Incr(k []byte, v uint64) (uint64, error) {
@@ -532,37 +297,6 @@ func (s *Store) Decr(k []byte, v uint64) (uint64, error) {
 	return s.chunks[idx].incrdecr(k, h, v, false)
 }
 
-// TODO - optimize
-func (c *chunk) incrdecr(k []byte, h uint32, v uint64, isIncr bool) (counter uint64, err error) {
-	mutex.Lock()
-	defer mutex.Unlock()
-	old, err := c.get(k, h)
-	if err == ErrNotFound {
-		//create empty counter
-		old = make([]byte, 8)
-		err = nil
-	}
-	if len(old) != 8 {
-		//better, then panic
-		return 0, errors.New("Unexpected value format")
-	}
-	if err != nil {
-		return
-	}
-	counter = binary.BigEndian.Uint64(old)
-	if isIncr {
-		counter += v
-	} else {
-		//decr
-		counter -= v
-	}
-	new := make([]byte, 8)
-	binary.BigEndian.PutUint64(new, counter)
-	err = c.set(k, new, h)
-
-	return
-}
-
 // Backup is very stupid now. It remove files with same name with bak extension
 // and create new backup files
 func (s *Store) Backup() (err error) {
@@ -573,31 +307,6 @@ func (s *Store) Backup() (err error) {
 		}
 	}
 	return
-}
-
-func (c *chunk) backup() (err error) {
-	c.Lock()
-	defer c.Unlock()
-	name := c.f.Name() + ".bak"
-	os.Remove(name)
-
-	dest, err := os.OpenFile(name, os.O_CREATE|os.O_RDWR, os.FileMode(fileMode))
-	if err != nil {
-		return
-	}
-	err = c.f.Sync()
-	if err != nil {
-		return
-	}
-	_, err = c.f.Seek(0, 0)
-	if err != nil {
-		return
-	}
-	if _, err = io.Copy(dest, c.f); err != nil {
-		return
-	}
-	dest.Sync()
-	return dest.Close()
 }
 
 func readUint32(b []byte) uint32 {
